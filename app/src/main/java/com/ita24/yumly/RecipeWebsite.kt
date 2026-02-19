8ab8603d4a23bd227914eb3bce30969b6f09908a
d22seba <sebastian3dobro@gmail.com>
2026-02-19 09:23:29 +0100

    Weiterleitung zur Rezept-Webseite fertig fehlen noch Buttons

---
+++ app/src/main/java/com/ita24/yumly/RecipeWebsite.kt
@@ -0,0 +1,21 @@
+package com.ita24.yumly
+
+import android.content.Intent
+import android.net.Uri
+import android.content.Context
+
+object RecipeWebsite{
+
+    fun getName(id: Int): String{
+        val url = "https://saschalf6.github.io/yumly-website/recipes/" + id + ".html"
+        return url
+    }
+
+    fun sendToWebsite(context: Context, id: Int){
+        val url = getName(id)
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        context.startActivity(intent)
+
+    }
+
+}
